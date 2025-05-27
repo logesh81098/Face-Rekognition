d1b2e2b4a2766b904c700e5ef0ccc771f25fe489
Logesh Shanmugavel <logesh81098@gmail.com>
2025-05-28 00:35:44 +0530

    Working with Application file

--- app.py
+++ app.py
@@ -4,23 +4,27 @@ import uuid
 import os
 
 app = Flask(__name__)
+
+# Constants
 UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+COLLECTION_ID = 'face-rekognition-collection'
+DYNAMODB_TABLE_NAME = 'Faceprints-Table'
 AWS_REGION = 'us-east-1'
 
+# Configurations
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+# AWS clients
 rekognition = boto3.client('rekognition', region_name='us-east-1')
 dynamodb = boto3.client('dynamodb', region_name='us-east-1')
-table = dynamodb.Table('Faceprints-Table')
-collection_id = 'face-rekognition-collection'
+table = dynamodb.Table(DYNAMODB_TABLE_NAME)
 
+# Save face info to DynamoDB
 def save_to_dynamo(face_id, image_name):
-    table.put_item(
-        Item={
-            'FaceId': face_id,
-            'ImageName': image_name
-        }
-    )
+    table.put_item(Item={'FaceId': face_id, 'ImageName': image_name})
 
+# Get all stored face IDs from DynamoDB
 def get_all_faces():
     response = table.scan()
     return [item['FaceId'] for item in response.get('Items', [])]
@@ -32,48 +36,25 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     if 'image' not in request.files:
-        return 'No file part'
-    
+        return 'No file part', 400
+
     file = request.files['image']
     if file.filename == '':
-        return 'No selected file'
-    
-    if file:
-        filename = str(uuid.uuid4()) + "_" + file.filename
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(filepath)
+        return 'No selected file', 400
 
-        with open(filepath, 'rb') as image:
-            response = rekognition.index_faces(
-                CollectionId=collection_id,
-                Image={'Bytes': image.read()},
-                ExternalImageId=filename,
-                DetectionAttributes=['DEFAULT']
-            )
+    # Save file locally
+    filename = str(uuid.uuid4()) + "_" + file.filename
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
 
-        if not response['FaceRecords']:
-            return 'No face detected.'
+    # Index the face using Rekognition
+    with open(filepath, 'rb') as image:
+        response = rekognition.index_faces(
+            CollectionId=COLLECTION_ID,
+            Image={'Bytes': image.read()},
+            ExternalImageId=filename,
+            DetectionAttributes=['DEFAULT']
+        )
 
-        face_id = response['FaceRecords'][0]['Face']['FaceId']
-        save_to_dynamo(face_id, filename)
-
-        matches = []
-        for face in get_all_faces():
-            if face == face_id:
-                continue  # skip self match
-
-            match_response = rekognition.search_faces(
-                CollectionId=collection_id,
-                FaceId=face_id,
-                FaceMatchThreshold=90,
-                MaxFaces=1
-            )
-
-            if match_response['FaceMatches']:
-                matches.append(match_response['FaceMatches'][0]['Face']['FaceId'])
-
-        return render_template('result.html', face_id=face_id, matches=matches)
-
-if __name__ == '__main__':
-    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-    app.run(host='0.0.0.0', port=81, debug=True)
+    if not response['FaceRecords']:
+        return 'No
